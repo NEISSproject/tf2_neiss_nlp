c51f2f154c86044827cab68d3f88ab579140139a
jochen <jochen.zoellner@planet-ai.de>
2021-06-29 13:18:33 +0200

    fix missing init

---
+++ tfaip_scenario/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 The neiss authors. All Rights Reserved.
+#
+# This file is part of tf2_neiss_nlp.
+#
+# tf2_neiss_nlp is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# tf2_neiss_nlp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the 1GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# tfaip. If not, see http://www.gnu.org/licenses/.
+# ==============================================================================
